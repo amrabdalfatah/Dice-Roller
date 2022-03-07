4c17a31cabd2e1cc7704e1d38db6caaa4aedd6e7
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-03-07 09:55:09 +0200

    Add ImageView

--- app/src/main/java/com/amrabdalfatah/diceroller/MainActivity.kt
+++ app/src/main/java/com/amrabdalfatah/diceroller/MainActivity.kt
@@ -3,8 +3,7 @@ package com.amrabdalfatah.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +17,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(drawableResource)
+
     }
 }
